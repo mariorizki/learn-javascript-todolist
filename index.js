b6e650d55321c59bd01a63c9a1222aea0de2fbb1
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-30 11:26:08 +0700

    Improve code quality

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-var todoList = {
-  todos: [],
+const todoList = {
+  todos: [
+    {
+      text: "Learning JavaScript",
+      completed: false,
+      datedAt: new Date("2019-01-01")
+      updatedAt: new Date("2019-01-01")
+    }
+  ],
+
   displayTodos: function() {
-    console.log('My todos: ', this.todos);
+    console.log('My todos: ', this.todos)
   },
+
   addTodo: function(todo) {
-    this.todos.push(todo);
-    this.displayTodos();
+    this.todos.push(todo)
+    this.displayTodos()
   },
+
   deleteTodo: function(position) {
-    this.todos.splice(position, 1);
-    this.displayTodos();
+    this.todos.splice(position, 1)
+    this.displayTodos()
   }
-};
+}
